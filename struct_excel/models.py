61379d5ba2bc8f3db46961250b509f0ed39c9c20
Fovir <fovir@disroot.org>
2026-05-18 23:18:13 +0800

    feat: transform models to `SQLModel`

--- struct_excel/models.py
+++ struct_excel/models.py
@@ -1,79 +1,77 @@
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
+from sqlmodel import Field, SQLModel
 
 
-class Gender(Enum):
+class Gender(str, Enum):
     MALE = "male"
     FEMALE = "female"
     OTHER = "other"
 
 
-class SessionMode(Enum):
+class CourseSessionMode(str, Enum):
     ONLINE = "online"
     OFFLINE = "offline"
 
 
-class Sector(Enum):
+class Sector(str, Enum):
     GOVERNMENT = "Government"
     PRIVATE = "Private"
     STUDENT = "Student"
 
 
-class PaymentStatus(Enum):
+class PaymentStatus(str, Enum):
     PAID = "PAID"
     PENDING = "PENDING"
 
 
-@dataclass
-class Student:
-    student_id: int
+class Student(SQLModel, table=True):
+    student_id: int | None = Field(default=None, primary_key=True)
     full_name: str
-    email: str
-    gender: Gender
+    email: str = Field(unique=True, index=True)
+    gender: Gender = Field(default=Gender.OTHER)
     it_background: bool
-    experience_min_years: int
-    experience_max_years: int
-    sector: Sector
-    supervisor_id: int | None
+    experience_min_years: int = Field(default=None)
+    experience_max_years: int = Field(default=None)
+    sector: Sector = Field(default=Sector.STUDENT)
+    supervisor_id: int | None = Field(
+        default=None, foreign_key="supervisor.supervisor_id"
+    )
     company: str
-    job_title: str | None
+    job_title: str | None = Field(default=None)
     country: str
     phone: str
 
 
-@dataclass
-class Course:
-    course_id: int
+class Course(SQLModel, table=True):
+    course_id: int | None = Field(default=None, primary_key=True)
     course_name: str
 
 
-@dataclass
-class Session:
-    session_id: int
-    course_id: int
+class CourseSession(SQLModel, table=True):
+    session_id: int | None = Field(default=None, primary_key=True)
+    course_id: int = Field(foreign_key="course.course_id")
     start_datetime: datetime
     end_datetime: datetime
-    mode: SessionMode
-    duration: float
+    mode: CourseSessionMode = Field(default=CourseSessionMode.OFFLINE)
+    duration: float = Field(default=0)
 
 
-@dataclass
-class Supervisor:
-    supervisor_id: int
+class Supervisor(SQLModel, table=True):
+    supervisor_id: int | None = Field(default=None, primary_key=True)
     full_name: str
-    email: str
+    email: str = Field(unique=True)
 
 
-@dataclass
-class Enrollment:
-    enrollment_id: int
-    student_id: int
-    session_id: int
+class Enrollment(SQLModel, table=True):
+    enrollment_id: int | None = Field(default=None, primary_key=True)
+    student_id: int = Field(foreign_key="student.student_id")
+    session_id: int = Field(foreign_key="coursesession.session_id")
     reg_date: datetime
-    completed: bool
-    payment_status: PaymentStatus
-    exception: str | None
+    completed: bool = Field(default=False)
+    payment_status: PaymentStatus = Field(default=PaymentStatus.PENDING)
+    exception: str | None = Field(default=None)
 
 
 @dataclass
@@ -100,6 +98,6 @@ class RawRow:
 @dataclass
 class CourseParseResult:
     datetime_range: list[tuple[datetime, datetime]]
-    mode: SessionMode
+    mode: CourseSessionMode
     course_name: str
     duration: float
