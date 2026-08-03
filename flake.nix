43638892a762fd2a937a6b40f3150b5bd38dbe10
Fovir <fovir@disroot.org>
2026-08-03 15:29:31 +0800

    chore: add dependencies of `numpy`
    Signed-off-by: Fovir <fovir@disroot.org>

--- flake.nix
+++ flake.nix
@@ -3,29 +3,36 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      # Add packages here.
-      buildInputs = with pkgs; [
-        (pkgs.python3.withPackages (python-pkgs:
-          with python-pkgs; [
-            # Python packages:
-          ]))
-        litecli
-        python313
-        sqlite
-      ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        # Add packages here.
+        buildInputs = with pkgs; [
+          (pkgs.python3.withPackages (
+            python-pkgs: with python-pkgs; [
+              # Python packages:
+            ]
+          ))
+          litecli
+          python313
+          sqlite
+          stdenv.cc.cc.lib
+          zlib
+        ];
+        LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib";
 
-      # Shell hooks.
-      shellHook = ''
-        echo "Entering the development environment!"
-      '';
+        # Shell hooks.
+        shellHook = ''
+          echo "Entering the development environment!"
+        '';
+      };
     };
-  };
 }
